851a19523a48c504222b4dc65d876ea3c17e70cc
gayathrikk <gayathri@htic.iitm.ac.in>
2024-04-05 14:30:36 +0530

    first commit

--- src/test/java/com/machines/Ping_Status/Login_Network_Ping.java
+++ src/test/java/com/machines/Ping_Status/Login_Network_Ping.java
@@ -3,7 +3,12 @@ package com.machines.Ping_Status;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,45 +16,60 @@ import java.io.InputStreamReader;
 
 public class Login_Network_Ping {
 	
-	  @Test
-	    public void testPing() {
-	        String domain = "humanbrain-annotation.firebaseapp.com";
-	        boolean isPingable = isDomainPingable(domain);
-	        if (isPingable) {
-	            System.out.println("Domain is pingable: " + domain);
-	        } else {
-	            System.out.println("Domain is not pingable: " + domain);
-	        }
-	        Assert.assertTrue(isPingable, "Domain is not pingable: " + domain);
+	  @Test(priority=1)
+	    public void Login_Link() throws Exception
+	    {
+	        connect("humanbrain-annotation.firebaseapp.com");
 	    }
+	  
+	
+	public static void connect(String... values) throws Exception {
+	    
+	    Session session = null;
+	    ChannelExec channel = null;
 
-	    private boolean isDomainPingable(String domain) {
-	        try {
-	            // Command to execute
-	            String command = "ping " + domain;
+	    try {
+	        session = new JSch().getSession("hbp", "apollo2.humanbrain.in", 22);
+	        session.setPassword("Health#123");
+	        session.setConfig("StrictHostKeyChecking", "no");
+	        session.connect();
+	        
+	        for(String s:values){
+	        if (channel != null) {
+	            channel.disconnect();
+	        } 
+	        channel = (ChannelExec) session.openChannel("exec"); 
+	        channel.setCommand("ping -c 7 "+s);
+	        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+	        channel.setOutputStream(responseStream);
+	        channel.connect();
+	        
+	        while (channel.isConnected()) {
+	            Thread.sleep(100);
+	        }
+	        
+	        String responseString = new String(responseStream.toByteArray());
+	        // System.out.println(responseString);
+	        String[] lines = responseString.split("\\r?\\n|\\r");
+	        try{
+	        String line = lines[lines.length-2];
+	        String[] str = line.split(", ", 3);
+	        System.out.println(lines[lines.length-3]);
+	        System.out.println(line);
+	        // System.out.println(str[1].charAt(0));
+	        int r = Integer.parseInt(String.valueOf(str[1].charAt(0)));
+	        Assert.assertNotEquals(r, 0);
+	        // System.out.println(r);
+	    }
+	        catch(Exception e){
+	            System.err.println("Unable to ping : "+s);
+	        }
 
-	            // Execute the command
-	            Process process = Runtime.getRuntime().exec(command);
-
-	            // Read the output
-	            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-	            String line;
-	            StringBuilder pingOutput = new StringBuilder();
-	            while ((line = reader.readLine()) != null) {
-	                pingOutput.append(line).append("\n");
-	            }
-
-	            // Wait for the command to finish execution
-	            process.waitFor();
-
-	            // Print the output
-	            System.out.println(pingOutput.toString());
-
-	            // Check if ping was successful
-	            return pingOutput.toString().contains("bytes=");
-	        } catch (IOException | InterruptedException e) {
-	            e.printStackTrace();
-	            return false; // If an exception occurs, consider domain as not pingable
+	    }
+	    } finally {
+	        if (session != null) {
+	            session.disconnect();
 	        }
 	    }
+	}
 }
